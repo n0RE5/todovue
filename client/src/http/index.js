e360250fda100a4fd5e26a562eb567b113151511
n0RE <nore@desktop>
2023-04-15 19:01:37 +0300

    update

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:5000/'
-
 const $api = axios.create({
-    baseURL: API_URL
+    baseURL: process.env.VUE_APP_API_URL
 })
 
 $api.interceptors.request.use(config => {
